69b48a28dd429a352fafe6de9aa811f686880e43
Luiz Eduardo da Silva Magalhaes <contato.luiz1997@gmail.com>
2024-12-26 23:01:47 -0300

    Aula 194

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+import express from 'express';
+import config from 'config';
+import router from './router';
+import db from '../config/db';
+
+const app = express();
+
+app.use(express.json());
+
+const port = config.get<number>('port');
+
+app.use("/api/", router);
+
+app.listen(port, async ()=>{
+    await db();
+    console.log('Aplicação está funcionando na porta: ' + port);
+});
+
